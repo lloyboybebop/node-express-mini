4e1f7298f866e8cc4e3e36b68597fc99568c553c
Lloyd Napier <lloyd.a.napier@gmail.com>
2019-02-27 18:43:46 -0800

    both GET

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // implement your API here
+const express = require('express'); // import express framework as a function
+const server = express(); // use express as server object
+server.use(express.json()); // json for the win
+
+const cors = require('cors');  // cross-origin resource sharing
+server.use(cors());
+
+const db = require('./data/db'); // db data
+
+const port = 9090;
+
+server.get('/', (req, res) => {
+    res.send('api up, yo');
+});
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then((users) => {
+        res.send(users);
+    })
+    .catch(() => {
+        res.status(500).send('That information cannot be retrieved')
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    db.findById(req.params.id)
+    .then((user) => {
+        if (user.length === 0) {
+            res.status(404).send('That user does not exist');
+        }
+        else {
+            res.status(200).send(user)
+        }
+    })
+    .catch(() => {
+        res.status(404).send('That user does not exist')
+    })
+});
+
+// server.post('/api/users, (req, res) => {
+
+
+
+// });
+
+server.listen(port, () => {   // waiting for request
+    console.log(`server is listening on port ${port}`); // response
+});
